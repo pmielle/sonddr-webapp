2892ead333570002c7d87befa8ca7bd4a385cbe8
Paul Mielle <pm.mielle@gmail.com>
2023-04-29 20:25:14 +0200

    fab service refactoring

--- src/app/services/fab.service.ts
+++ src/app/services/fab.service.ts
@@ -3,6 +3,7 @@ import { FabMode, goalMode, homeMode } from '../interfaces/fab-mode';
 import { TabService } from './tab.service';
 import { Subscription, filter, map } from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
+import { Tab } from '../interfaces/tab';
 
 @Injectable({
   providedIn: 'root'
@@ -32,33 +33,39 @@ export class FabService implements OnDestroy {
 
   // methods
   // --------------------------------------------
+  setModeStack(mode: FabMode|undefined, tab?: Tab) {  // hypothesis: tabStacks are always init with only 1 element
+    if (tab === undefined) {
+      tab = this.tab.defaultTab;
+    }
+    this.tabStacks[tab.name] = [mode];
+  }
+
   _onRouteChange(): Subscription {
     return this.router.events.pipe(
       filter(e => e instanceof NavigationEnd),
-      map(e => e as NavigationEnd),  // rxjs does not understand otherwise
+      map(e => e as NavigationEnd),  // otherwise, rxjs does not understand
     ).subscribe((e) => {
-      let newMode: FabMode|undefined;
-      switch(e.urlAfterRedirects) {
-        case "/goal": 
-          newMode = goalMode;
-          break;
-        case "/":
-          newMode = homeMode;
-          break;
-        default:
-          console.error(`Unexpected route ${e.urlAfterRedirects}: cannot set the correct fab mode`);
-          newMode = undefined;
-      }
-      this.tabStacks[this.tab.defaultTab.name] = [newMode];
+      let newMode = this._chooseFabOfRoute(e.urlAfterRedirects);
+      this.setModeStack(newMode);
     });
+  }
+
+  _chooseFabOfRoute(route: string): FabMode|undefined {
+    switch(route) {
+      case "/goal": 
+        return goalMode;
+      case "/":
+        return homeMode;
+        break;
+      default:
+        console.error(`Unexpected route ${route}: cannot set the correct fab mode`);
+        return undefined;
+    }
   }
 
   _initTabStacks() {
     this.tab.tabs.forEach((t) => {
-      this.tabStacks[t.name] = [];
-      if (t.fab) {
-        this.tabStacks[t.name].push(t.fab);
-      }
+      this.setModeStack(t.fab, t);
     });
   }
 }
