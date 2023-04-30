7365eb55542bd3749491619a452708061dd6b882
Paul Mielle <pm.mielle@gmail.com>
2023-04-30 09:08:40 +0200

    hscroll widht shadows

--- src/app/components/h-scroll/h-scroll.component.ts
+++ src/app/components/h-scroll/h-scroll.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-h-scroll',
   templateUrl: './h-scroll.component.html',
   styleUrls: ['./h-scroll.component.scss']
 })
-export class HScrollComponent {
+export class HScrollComponent implements OnInit, AfterViewInit {
 
+  // attributes
+  // --------------------------------------------
+  @ViewChild('container') containerRef!: ElementRef;
+  containerElem?: HTMLElement;
+  leftShadow = false;
+  rightShadow = true;
+
+  // lifecycle hooks
+  // --------------------------------------------
+  ngOnInit() { }
+  
+  ngAfterViewInit() {
+    this._findContainer();
+  }
+  
+  // methods
+  // --------------------------------------------
+  onScroll() {
+    if (this.containerElem === undefined) {
+      console.warn("#container not found, cannot handle scroll. This might happen if scroll happens before _findContainer has run");
+      return;
+    }
+    this._manageShadows(this.containerElem.scrollLeft, this.containerElem.clientWidth, this.containerElem.scrollWidth);
+  }
+
+  _manageShadows(scrollLeft: number, width: number, scrollWidth: number) {
+    // left
+    if (scrollLeft > 0) {
+      if (this.leftShadow === false) { this.leftShadow = true; }
+    } else {
+      if (this.leftShadow === true) { this.leftShadow = false; }
+    }
+    // right
+    if (scrollLeft + width < scrollWidth) {
+      if (this.rightShadow === false) { this.rightShadow = true; }
+    } else {
+      if (this.rightShadow === true) { this.rightShadow = false; }
+    }
+  }
+
+  _findContainer() {
+    this.containerElem = this.containerRef.nativeElement;    
+  }
 }
