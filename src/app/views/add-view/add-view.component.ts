b1f3f0b890e3476eb431d22f33f221bd455b6ae9
Paul Mielle <pm.mielle@gmail.com>
2023-05-03 11:16:10 +0200

    preselect goal

--- src/app/views/add-view/add-view.component.ts
+++ src/app/views/add-view/add-view.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Goal } from 'src/app/interfaces/goal';
+import { DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-add-view',
@@ -7,4 +11,46 @@ import { Component } from '@angular/core';
 })
 export class AddViewComponent {
 
+  // dependencies
+  // --------------------------------------------
+  route = inject(ActivatedRoute);
+  db = inject(DatabaseService);
+
+  // attributes
+  // --------------------------------------------
+  queryParamSub?: Subscription;
+  goals: Goal[] = [];
+  selectedGoals: Goal[] = [];
+
+  // lifecycle hooks
+  // --------------------------------------------
+  constructor() {
+    this._loadGoals().then(() => {
+      this.queryParamSub = this._subscribeToQueryParam();
+    });
+  }
+
+  // methods
+  // --------------------------------------------
+  async _loadGoals() {
+    this.goals = await this.db.getGoals();
+  }
+
+  _selectGoal(id: string) {    
+    let matchingGoal = this.goals.find((g) => g.id === id);
+    if (!matchingGoal) {
+      console.error(`${id} not found in this.goals`);
+      return;
+    }
+    this.selectedGoals = [matchingGoal];
+  }
+
+  _subscribeToQueryParam(): Subscription {
+    return this.route.queryParamMap.subscribe((param) => {
+      let goalId = param.get("preselectedGoal");
+      if (goalId) {
+        this._selectGoal(goalId);
+      }
+    })
+  }
 }
--- src/app/views/goal-view/goal-view.component.ts
+++ src/app/views/goal-view/goal-view.component.ts
@@ -87,7 +87,6 @@ export class GoalViewComponent implements OnDestroy {
       }
       resolve();
     });
-    
   }
 
   async _onideaOrderByChange() {
