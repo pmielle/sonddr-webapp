2a82c7ee24ffbf89f376b8741b0f36316d2d5476
Paul Mielle <pm.mielle@gmail.com>
2023-08-26 15:34:23 +0200

    cleaner shadown management

--- src/app/components/h-scroll/h-scroll.component.ts
+++ src/app/components/h-scroll/h-scroll.component.ts
@@ -1,19 +1,17 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-h-scroll',
   templateUrl: './h-scroll.component.html',
   styleUrls: ['./h-scroll.component.scss']
 })
-export class HScrollComponent implements OnInit, AfterViewInit {
+export class HScrollComponent implements OnInit {
 
   // attributes
   // --------------------------------------------
   @Input('left-padding') leftPadding: string = "0px";
   @Input('top-padding') topPadding: string = "0px";
   @Input('hide-shadows') hideShadows: boolean = false;
-  @ViewChild('container') containerRef!: ElementRef;
-  containerElem?: HTMLElement;
   leftShadow = false;
   rightShadow = true;
 
@@ -21,36 +19,8 @@ export class HScrollComponent implements OnInit, AfterViewInit {
   // --------------------------------------------
   ngOnInit() { }
   
-  ngAfterViewInit() {
-    this._findContainer();
-  }
-  
   // methods
   // --------------------------------------------
-  onScroll() {
-    if (this.containerElem === undefined) {
-      console.warn("#container not found, cannot handle scroll. This might happen if scroll happens before _findContainer has run");
-      return;
-    }
-    this._manageShadows(this.containerElem.scrollLeft, this.containerElem.clientWidth, this.containerElem.scrollWidth);
-  }
+  // ...
 
-  _manageShadows(scrollLeft: number, width: number, scrollWidth: number) {
-    // left
-    if (scrollLeft > 0) {
-      if (this.leftShadow === false) { this.leftShadow = true; }
-    } else {
-      if (this.leftShadow === true) { this.leftShadow = false; }
-    }
-    // right
-    if (scrollLeft + width < scrollWidth) {
-      if (this.rightShadow === false) { this.rightShadow = true; }
-    } else {
-      if (this.rightShadow === true) { this.rightShadow = false; }
-    }
-  }
-
-  _findContainer() {
-    this.containerElem = this.containerRef.nativeElement;    
-  }
 }
