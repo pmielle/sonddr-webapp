49e7b23ce705819702495af34ff144ab5bc2a976
Paul Mielle <pm.mielle@gmail.com>
2023-08-26 15:52:22 +0200

    padding fixes

--- src/app/components/h-scroll/h-scroll.component.ts
+++ src/app/components/h-scroll/h-scroll.component.ts
@@ -5,19 +5,17 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './h-scroll.component.html',
   styleUrls: ['./h-scroll.component.scss']
 })
-export class HScrollComponent implements OnInit {
+export class HScrollComponent {
 
   // attributes
   // --------------------------------------------
-  @Input('left-padding') leftPadding: string = "0px";
-  @Input('top-padding') topPadding: string = "0px";
   @Input('hide-shadows') hideShadows: boolean = false;
   leftShadow = false;
   rightShadow = true;
 
   // lifecycle hooks
   // --------------------------------------------
-  ngOnInit() { }
+  constructor() { }
   
   // methods
   // --------------------------------------------
