412ef66a3351904d609d445a88fb963b9031334a
Paul Mielle <pm.mielle@gmail.com>
2023-09-27 07:05:29 +0200

    preselected in add view works

--- src/app/views/add-view/add-view.component.ts
+++ src/app/views/add-view/add-view.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription, combineLatest, from } from 'rxjs';
+import { Goal, Idea } from 'sonddr-shared';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { ScreenSizeService } from 'src/app/services/screen-size.service';
 
 @Component({
   selector: 'app-add-view',
@@ -6,5 +12,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-view.component.scss']
 })
 export class AddViewComponent {
+
+  // dependencies
+  // --------------------------------------------
+  route = inject(ActivatedRoute);
+  api = inject(ApiService);
+  screen = inject(ScreenSizeService);
+  auth = inject(AuthService);
+  
+  // attributes
+  // --------------------------------------------
+  mainSub?: Subscription;
+  ideas?: Idea[];
+  goals?: Goal[];
+  selectedGoals = new Set<Goal>();
+
+  // lifecycle hooks
+  // --------------------------------------------
+  ngOnInit(): void {
+    this.api.getGoals().then(g => this.goals = g);
+    
+    this.mainSub = combineLatest([
+      this.route.queryParamMap,
+      from(this.api.getGoals()),
+    ]).subscribe(([map, goals]) => {
+
+      // get all goals
+      this.goals = goals;
+
+      // preselect a goal if query param
+      const id = map.get("preselected");
+      if (id) {
+        const goal = goals.find(g => g.id === id);
+        if (!goal) {
+          throw new Error(`Failed to preselect ${id}: no matching goal found`);
+        }
+        this.selectedGoals.add(goal);
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.mainSub?.unsubscribe();
+  }
+
+  // methods
+  // --------------------------------------------
+  // ...
 
 }
