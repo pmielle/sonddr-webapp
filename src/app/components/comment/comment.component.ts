fdce19e3ce5948271b90b5a4db99f453669e0d05
Paul Mielle <pm.mielle@gmail.com>
2023-09-29 18:50:43 +0200

    comment ui

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Comment } from 'sonddr-shared';
+import { TimeService } from 'src/app/services/time.service';
 
 @Component({
   selector: 'app-comment',
@@ -8,6 +9,22 @@ import { Comment } from 'sonddr-shared';
 })
 export class CommentComponent {
 
+  // dependencies
+  // --------------------------------------------
+  time = inject(TimeService);
+
+  // I/O
+  // --------------------------------------------
   @Input('comment') comment?: Comment;
+
+  // methods
+  // --------------------------------------------
+  chooseRatingColor(): string {
+    if (!this.comment) { return ""; }
+    if (this.comment.rating === 0) { return "" };
+    return this.comment.rating > 0
+      ? "var(--green)"
+      : "var(--red)";
+  }
 
 }
