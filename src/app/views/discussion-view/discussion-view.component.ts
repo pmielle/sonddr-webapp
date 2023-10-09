613853c496294ec554306d881c8c055749fa979d
Paul Mielle <pm.mielle@gmail.com>
2023-10-09 15:37:35 +0200

    log

--- src/app/views/discussion-view/discussion-view.component.ts
+++ src/app/views/discussion-view/discussion-view.component.ts
@@ -42,7 +42,6 @@ export class DiscussionViewComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.routeSub?.unsubscribe();
-    console.log("restoreeeeee");
     this.mainNav.restoreNavBar();
   }
 
