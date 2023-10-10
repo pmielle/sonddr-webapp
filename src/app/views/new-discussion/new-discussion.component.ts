49ecd24aa472bc79737444a63889351252640ae2
Paul Mielle <pm.mielle@gmail.com>
2023-10-10 14:50:34 +0200

    get preselected from query params

--- src/app/views/new-discussion/new-discussion.component.ts
+++ src/app/views/new-discussion/new-discussion.component.ts
@@ -1,4 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { User } from 'sonddr-shared';
+import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { ScreenSizeService } from 'src/app/services/screen-size.service';
 
@@ -7,11 +11,38 @@ import { ScreenSizeService } from 'src/app/services/screen-size.service';
   templateUrl: './new-discussion.component.html',
   styleUrls: ['./new-discussion.component.scss']
 })
-export class NewDiscussionComponent {
+export class NewDiscussionComponent implements OnInit, OnDestroy {
 
   // dependencies
   // --------------------------------------------
   auth = inject(AuthService);
   screen = inject(ScreenSizeService);
+  route = inject(ActivatedRoute);
+  api = inject(ApiService);
+
+  // attributes
+  // --------------------------------------------
+  routeSub?: Subscription;
+  selectedUser?: User;
+
+  // lifecycle hooks
+  // --------------------------------------------
+  ngOnInit(): void {
+    this.routeSub = this.route.queryParamMap.subscribe(async map => {
+      const id = map.get("preselected");
+      if (id) {
+        const user = await this.api.getUser(id);
+        this.selectedUser = user;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe;
+  }
+
+  // methods
+  // --------------------------------------------
+  // ...
   
 }
