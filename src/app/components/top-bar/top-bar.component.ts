7a912eed20190dfa8707c85977bd6387c3156035
Paul Mielle <pm.mielle@gmail.com>
2023-10-10 17:55:04 +0200

    scroll to top on top bar click

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, inject } from '@angular/core';
 import { Location } from '@angular/common';
 import { ScreenSizeService } from 'src/app/services/screen-size.service';
 import { ColorService } from 'src/app/services/color.service';
+import { MainNavService } from 'src/app/services/main-nav.service';
 
 type LeftItem = "back" | "close" | "logo";
 
@@ -17,11 +18,13 @@ export class TopBarComponent {
   screen = inject(ScreenSizeService);
   location = inject(Location);
   color = inject(ColorService);
+  mainNav = inject(MainNavService);
   
   // i/o
   // --------------------------------------------
   @Input("left-item") leftItem?: LeftItem;
   @Input("background-color") backgroundColor: string = "#303030";  // has to be hardcoded hex because colorservice
+  @Input("reverse-scroll") reverseScroll = false;
 
   // attributes
   // --------------------------------------------
@@ -29,6 +32,12 @@ export class TopBarComponent {
   
   // methods
   // --------------------------------------------
-  // ...
+  onClick() {
+    if (this.reverseScroll) {
+      this.mainNav.scrollToBottom(true);
+    } else {
+      this.mainNav.scrollToTop(true);
+    }
+  }
 
 }
