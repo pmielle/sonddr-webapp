b39a0747848d29c79f3d232c903f16b66b0537bd
Paul Mielle <pm.mielle@gmail.com>
2024-02-29 07:15:38 +0100

    fab animation from void

--- src/app/components/fab/fab.component.ts
+++ src/app/components/fab/fab.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, inject } from '@angular/core';
 import { MainNavService } from 'src/app/services/main-nav.service';
 import { ScreenSizeService } from 'src/app/services/screen-size.service';
@@ -7,7 +8,18 @@ import { ScreenSizeService } from 'src/app/services/screen-size.service';
 @Component({
   selector: 'app-fab',
   templateUrl: './fab.component.html',
-  styleUrls: ['./fab.component.scss']
+  styleUrls: ['./fab.component.scss'],
+  animations: [
+    trigger('fadeSlideInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(10px)' }),
+        animate('100ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('100ms ease-in', style({ opacity: 0, transform: 'translateX(10px)' })),
+      ]),
+    ]),
+  ],
 })
 export class FabComponent {
 
